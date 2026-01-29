6839d984cfcb647f2901034a0e96c915fc2854df
nonyponymlg-png <nonyponymlg@gmail.com>
2026-01-29 11:03:36 -0600

    Update Train.java

--- 102-Train/Train.java
+++ 102-Train/Train.java
@@ -3,6 +3,25 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Train{
+    int x;
+    int y;
+    ArrayList<RailCar> cars = new ArrayList<RailCar>();
 
+    public Train(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
 
+    void addCar(String type, Color color) {
+        cars.add(cars.size(), new RailCar(type, color));
+    }
+    void addCar(int i,String type, Color color) {
+        cars.add(i,new RailCar(type, color));
+    }
+
+    void showCars(Graphics g) {
+        for (int index = 0; index < cars.size(); index++) {
+            cars.get(index).draw(g,(index*100) + x,y);
+        }
+    }
 }
