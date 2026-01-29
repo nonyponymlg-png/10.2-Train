a47b3a0114e579e48609efd105c8774f79429532
nonyponymlg-png <nonyponymlg@gmail.com>
2026-01-29 11:04:37 -0600

    Update paintComponent to include train cars
    Added train cars to the paintComponent method for display.

--- 102-Train/Display.java
+++ 102-Train/Display.java
@@ -5,11 +5,18 @@ import java.util.ArrayList;
 
 public class Display extends JPanel{
 	public Display(){}
-	// paintCommponent is not explicitly called. It is executed whenever the JPanel is called.
-	// Graphics g object is implicitly passed to function.
 	public void paintComponent(Graphics g){
-		super.paintComponent(g); //satisfies the parent class' requirements.
+		super.paintComponent(g); 
 		setBackground(Color.WHITE);
-
+		Train train = new Train(0,0); 
+		train.addCar("Locomotive",Color.blue); 
+		train.addCar("PassengerCar",Color.gray); 
+		train.addCar("PassengerCar",Color.gray); 
+		train.addCar("FreightCar",Color.green); 
+		train.addCar(3,"PassengerCar",Color.gray); 
+		train.addCar("FreightCar",Color.green); 
+		train.addCar("Caboose",Color.red); 
+		train.addCar(6,"FreightCar",Color.green); 
+		train.showCars(g); 
 	}
 }
